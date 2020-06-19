26510b45b7336a0860fbe6d42cee6517ec26562e
zhoujianping <zhoujianping@jiuxiniot.com>
2020-06-19 18:12:17 +0800

    gin -- gomicro++

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/ops-cn/admin v0.0.0-20200616055503-4ce04eec80ea
-	github.com/ops-cn/common v0.0.0-20200618122247-50e86a7a60ad // indirect
+	github.com/ops-cn/common v0.0.0-20200618122247-50e86a7a60ad
+	github.com/ops-cn/proto v0.0.0-20200618105558-fb5d56e0d095
 )
--- handler/admin/a_login.go
+++ handler/admin/a_login.go
@@ -1,17 +1,35 @@
 package admin
 
 import (
+	"github.com/golang/protobuf/ptypes"
 	"github.com/ops-cn/admin/app/ginplus"
+	"github.com/ops-cn/common/captcha"
 	"github.com/ops-cn/common/config"
+	proto "github.com/ops-cn/proto/admin/login"
+	"github.com/ops-cn/proto/unified"
 	"net/http"
 )
 
-func Login(w http.ResponseWriter, r *http.Request) {
-	ctx := c.Request.Context()
-	item, err := a.LoginBll.GetCaptcha(ctx, config.C.Captcha.Length)
+func GetCaptcha(w http.ResponseWriter, r *http.Request) unified.Response {
+
+	ctx := r.Context()
+	captchaID := captcha.NewLen(config.C.Captcha.Length)
+
+	item := &proto.LoginCaptcha{
+		CaptchaID: captchaID,
+	}
+
+	any, err := ptypes.MarshalAny(item)
+	return unified.Response{
+		TraceId: "",
+		Status:  0,
+		Desc:    "",
+		Items:   any,
+	}
+
 	if err != nil {
-		ginplus.ResError(c, err)
+		ginplus.ResError(ctx, err)
 		return
 	}
-	ginplus.ResSuccess(c, item)
+	ginplus.ResSuccess(ctx, item)
 }
